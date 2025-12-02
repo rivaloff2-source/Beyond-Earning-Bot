16ee7db0aa417ddf3e36982559a1f7d7bbd08ca4
rivaloff2-source <rivaloff2@gmail.com>
2025-12-02 05:35:35 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -234,11 +234,10 @@ def setup_handlers(bot_instance, admin_ids, required_channel, contact_bot):
                         else:
                             BOT.send_message(c.message.chat.id, item["text"])
                 
-                if is_admin(uid):
-                    BOT.send_message(c.message.chat.id, "Use admin panel to delete or add more.", reply_markup=loot_actions_kb(lid))
-                else:
-                    BOT.send_message(c.message.chat.id, "More options:", reply_markup=loot_actions_kb(lid))
-                return
+            if is_admin(uid):
+                BOT.send_message(c.message.chat.id, "Use admin panel to delete or add more.", reply_markup=loot_actions_kb(lid))
+            else:
+                BOT.send_message(c.message.chat.id, "More options:", reply_markup=loot_actions_kb(lid))
         
             if data == "menu_doubt":
                 MY_CONTACT = "@Beyond_Unknown1"  # Your username
@@ -249,6 +248,7 @@ def setup_handlers(bot_instance, admin_ids, required_channel, contact_bot):
                     reply_markup=back_button("back_main")
                 )
                 return
+            return
 
             if data == "menu_admin":
                 if not is_admin(uid):
